e59f19f0bfb3e22e4d68dd717e7f6910c68dd60d
Ignacio Castillo <ignacio.bcastillo@gmail.com>
2026-04-01 21:11:26 -0600

    add get_by_username

--- ppml/repositories/__init__.py
+++ ppml/repositories/__init__.py
@@ -16,6 +16,7 @@ class UsersProfilesRepository:
                 email      = email,
                 first_name = first_name,
                 last_name  = last_name,
+                is_disabled = False
             )
             return Ok(profile)
         except Exception as e:
